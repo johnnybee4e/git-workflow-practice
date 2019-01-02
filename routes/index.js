f3aa18a87edbd861a0c38842c786eb3d217c5a68
Jon Rosado <jon.rosado42@gmail.com>
2019-01-01 21:39:22 -0500

    Change title key in res.render of index route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'This is an Express experiment' });
 });
 
 module.exports = router;
